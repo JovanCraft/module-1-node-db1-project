6e192824277fdb858360ac24f569df31a105f035
jovan <jovancraft@gmail.com>
2024-01-26 03:37:37 -0600

    finished for the most part

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -16,7 +16,7 @@ router.get('/', async (req, res, next) => {
   }
 })
 
-router.get('/:id', checkAccountId, async (req, res, next) => {
+router.get('/:id', checkAccountId, async (req, res, next) => {// eslint-disable-line
   // DO YOUR MAGIC
   // try {
   //   const account = await Accounts.getById(req.params.id)
