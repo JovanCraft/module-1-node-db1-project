1342ee942e6b53d64d93ab5a43db469ae5cca87d
jovan <jovancraft@gmail.com>
2024-01-26 01:12:55 -0600

    made sure all of my endpoints worked

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -2,26 +2,52 @@ const router = require('express').Router()
 
 router.get('/', (req, res, next) => {
   // DO YOUR MAGIC
+  try {
+    res.json(`get account: GET ALL`)
+  }catch(err){
+    next(err)
+  }
 })
 
 router.get('/:id', (req, res, next) => {
   // DO YOUR MAGIC
+  try {
+    res.json(`get account: GET BY ID`)
+  }catch(err){
+    next(err)
+  }
 })
 
 router.post('/', (req, res, next) => {
   // DO YOUR MAGIC
+  try {
+    res.json(`get account: POST NEW ACCOUNT`)
+  }catch(err){
+    next(err)
+  }
 })
 
 router.put('/:id', (req, res, next) => {
   // DO YOUR MAGIC
+  try {
+    res.json(`get account: UPDATE ACCOUNT BY ID`)
+  }catch(err){
+    next(err)
+  }
 });
 
 router.delete('/:id', (req, res, next) => {
   // DO YOUR MAGIC
+  try {
+    res.json(`get account: DELETE ACCOUNT BY ID`)
+  }catch(err){
+    next(err)
+  }
 })
 
 router.use((err, req, res, next) => { // eslint-disable-line
   // DO YOUR MAGIC
+  res.status(err.status || 500).json({ message: err.message })
 })
 
 module.exports = router;
